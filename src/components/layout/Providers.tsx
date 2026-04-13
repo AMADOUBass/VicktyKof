7147c69a95b5791d5fe412c115199dad90811acb
Amadou Bassoum <bassoumamadou0@gmail.com>
2026-04-13 16:26:24 -0400

    chore: full integration of PostHog and manual Sentry setup

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -4,6 +4,8 @@ import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
+import { PHProvider } from "./PostHogProvider";
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
     () =>
@@ -15,8 +17,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
   );
 
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </SessionProvider>
+    <PHProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </SessionProvider>
+    </PHProvider>
   );
 }
